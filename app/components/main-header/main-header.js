896b422c21d58d9e9a99cbc2f6ee0bdd040268af
Burcu <burcu@MacBook-Air-von-Burcu.local>
2024-05-02 13:41:20 +0200

    Making active link is refactored

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -1,12 +1,10 @@
-"use client";
-import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
+import Link from "next/link";
 export default function MainHeader() {
-  const path = usePathname();
   return (
     <>
       <MainHeaderBackground></MainHeaderBackground>
@@ -18,20 +16,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link
-                href="/meals"
-                className={
-                  path.startsWith("/meals") ? classes.active : undefined
-                }
-              >
-                Browse Meals
-              </Link>
+              <NavLink href="/meals"> Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">
-                Foodies Community className=
-                {path.startsWith("/community") ? classes.active : undefined}
-              </Link>
+              <NavLink href="/community"> Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
