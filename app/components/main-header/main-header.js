de3c8361181149d95e00c853bd4995785684bb13
Burcu <burcu@MacBook-Air-von-Burcu.local>
2024-05-02 13:30:14 +0200

    Links are made active

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -1,9 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
 export default function MainHeader() {
+  const path = usePathname();
   return (
     <>
       <MainHeaderBackground></MainHeaderBackground>
@@ -15,10 +18,20 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                }
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link href="/community">
+                Foodies Community className=
+                {path.startsWith("/community") ? classes.active : undefined}
+              </Link>
             </li>
           </ul>
         </nav>
