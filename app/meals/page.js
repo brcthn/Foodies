abaa59a275c38d2d5d5e99b95ebdc0dac6445f71
Burcu <burcu@MacBook-Air-von-Burcu.local>
2024-05-02 14:21:51 +0200

    meal items are added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,24 @@
 import Link from "next/link";
-
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <p>
-        <Link href="/meals/type-1"> Meal-type-1</Link>
-      </p>
-      <p>
-        <Link href="/meals/type-2"> Meal-type-2</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{""}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}></MealsGrid>
+      </main>
+    </>
   );
 }
