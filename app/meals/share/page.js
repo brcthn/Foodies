2b816d7402033f6a8a7798c9f5f420094c9039b2
Burcu <burcu@MacBook-Air-von-Burcu.local>
2024-05-06 10:54:17 +0200

    Form is submitted

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,8 @@
+"use client";
 import ImagePicker from "@/app/components/meals/image-picker";
 import classes from "./page.module.css";
-
+import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/app/components/meals/meals-form-submit";
 export default function ShareMealPage() {
   return (
     <>
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,9 +41,9 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker></ImagePicker>
+          <ImagePicker label="Your image" name="image"></ImagePicker>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit></MealsFormSubmit>
           </p>
         </form>
       </main>
